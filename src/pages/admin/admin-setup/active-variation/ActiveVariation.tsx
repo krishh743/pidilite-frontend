8a29f3446bb52c59b4a0022d8cca8fd3b531fe85
krishna <krishnakushwaha743@gmail.com>
2024-06-26 23:23:47 +0530

    combine both form

--- src/pages/admin/admin-setup/active-variation/ActiveVariation.tsx
+++ src/pages/admin/admin-setup/active-variation/ActiveVariation.tsx
@@ -2,10 +2,12 @@ import React, { useEffect, useState, useCallback } from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { IoEyeOutline } from "react-icons/io5";
 import { ToastContainer } from "react-toastify";
-import { Button, Form, Input, Select } from "antd";
+import { Button, Form, Input, Modal, Select, Upload, UploadProps } from "antd";
 import { GetGamesListAPi } from "../../../../services/ApiServices";
 import UploadAndViewVariationForm from "./UploadAndViewVariationForm";
-import KbcQuestionFormUpload from "./KbcQuestionFromUpload";
+import { PlusOutlined } from "@ant-design/icons";
+
+type FileType = Parameters<NonNullable<UploadProps["beforeUpload"]>>[0];
 
 const { Option } = Select;
 
@@ -20,6 +22,7 @@ interface IQuestions {
   questionImage: string;
   options: IOptions[];
 }
+
 
 interface gameOverview {
   id: number | null | string;
@@ -66,17 +69,17 @@ const defaultGameOverview: gameOverview = {
     questions: [
       {
         question: "",
-        correctAnswer: "",
         questionImage: "",
         options: [
           {
             option: "",
-            optionImage: ""
+            optionImage: "",
           }
-        ]
-
+        ],
+        correctAnswer: ""
       }
-    ],
+      ],
+
     img2: "",
     img6: "",
     img12: "",
@@ -98,6 +101,17 @@ function ActiveVariation() {
   const [currentPage, setCurrentPage] = useState(1);
   const [gameListData, setGameListData] = useState([]);
   const [openedGame, setOpenedGame] = useState(defaultGameOverview);
+  const [fileLists, setFileLists] = useState({
+    siteBanner: [],
+    mobileBanner: [],
+    productImage: [],
+    img2: [],
+  });
+  const [previewVisible, setPreviewVisible] = useState(false);
+  const [previewImage, setPreviewImage] = useState<string | undefined>(
+    undefined
+  );
+  const [togleForm, setToggleForm] = useState("");
 
   const rowsPerPage = 10;
   const totalPages = Math.ceil(gameListData.length / rowsPerPage);
@@ -124,7 +138,17 @@ function ActiveVariation() {
       ...prevState,
       gameType: value,
     }));
+    setToggleForm(value);
   }, []);
+
+  const handleVariationNameInputChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setOpenedGame((prevState) => ({
+      ...prevState,
+      variationName: e.target.value,
+    }));
+  };
 
   const handleNextPage = useCallback(() => {
     setCurrentPage((prev) => Math.min(prev + 1, totalPages));
@@ -155,6 +179,19 @@ function ActiveVariation() {
         backgroundImage: "",
         trainerBackgroundImage: "",
         playerBackgroundImage: "",
+        questions: [
+          {
+            question: "",
+            correctAnswer: "",
+            questionImage: "",
+            options: [
+              {
+                option: "",
+                optionImage: "",
+              },
+            ],
+          },
+        ],
         img2: "",
         img6: "",
         img12: "",
@@ -173,12 +210,39 @@ function ActiveVariation() {
     });
   };
 
+  const handleChange =
+    (imageType: any) =>
+    ({ fileList }: any) => {
+      setFileLists((prevFileLists) => ({
+        ...prevFileLists,
+        [imageType]: fileList,
+      }));
+    };
+
+  const getBase64 = (file: FileType): Promise<string> =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+    });
+  const handlePreviewImage = async (file: any) => {
+    if (file.url) {
+      setPreviewImage(file.url);
+      setPreviewVisible(true);
+      return;
+    }
+    const base64 = await getBase64(file.originFileObj as FileType);
+    setPreviewImage(base64);
+    setPreviewVisible(true);
+  };
+
   const handleCancelGameDetailsUpdates = useCallback(() => {
     setOpenedGame(defaultGameOverview);
   }, []);
 
   return (
-    <div>
+    <div> 
       <ToastContainer />
       <div className="flex gap-4">
         <div>
@@ -220,7 +284,8 @@ function ActiveVariation() {
                           {index + 1}
                         </td>
                         <td className="border-b border-[#eee] py-4 px-4 dark:border-strokedark text-left">
-                          {row?.id === 2 ? "SNL" : "KBC"}
+                          {row?.gameType === "snl" ? "S&L" : "KBC"}
+
                         </td>
                         <td className="border-b border-[#eee] pl-10 dark:border-strokedark">
                           {row?.variationName}
@@ -265,11 +330,9 @@ function ActiveVariation() {
           ) : (
             <div className="w-full">
               <div className="flex direction-row justify-end gap-4 items-center mb-4">
-
                 <Button className="text-sm bg-black text-white font-bold hover:bg-blue-700 px-4 rounded-md">
                   Save
                 </Button>
-
 
                 <Button
                   onClick={handleCancelGameDetailsUpdates}
@@ -289,6 +352,7 @@ function ActiveVariation() {
                       <Input
                         placeholder="Enter something..."
                         value={openedGame?.variationName}
+                        onChange={handleVariationNameInputChange}
                       />
                     </Form.Item>
                     <Form.Item label="Game Type">
@@ -301,21 +365,82 @@ function ActiveVariation() {
                         <Option value="kbc">KBC</Option>
                       </Select>
                     </Form.Item>
+                    <div className="flex flex-col justify-between gap-4 w-full"></div>
                   </Form>
 
-                  {/* add basic details banners */}
-                  
-                  
-                  {openedGame.gameType === "snl" ? (
-                    <UploadAndViewVariationForm openedGame={openedGame} />
-                  ) : (
-                    <KbcQuestionFormUpload />
-                  )}
+                  <div className="flex justify-between">
+                    <h3>Add Site Banner (1280 px by 150 px)</h3>
+                    <Upload
+                      action="/api/upload"
+                      listType="picture-card"
+                      fileList={fileLists.siteBanner}
+                      onChange={handleChange("siteBanner")}
+                      onPreview={handlePreviewImage}
+                    >
+                      {fileLists.siteBanner.length < 1 && (
+                        <div>
+                          <PlusOutlined />
+                          <div style={{ marginTop: 8 }}>Upload</div>
+                        </div>
+                      )}
+                    </Upload>
+                  </div>
+
+                  <div className="flex justify-between">
+                    <h3>Add Product Image</h3>
+
+                    <Upload
+                      action="/api/upload"
+                      listType="picture-card"
+                      fileList={fileLists.mobileBanner}
+                      onChange={handleChange("mobileBanner")}
+                      onPreview={handlePreviewImage}
+                    >
+                      {fileLists.mobileBanner.length < 1 && (
+                        <div>
+                          <PlusOutlined />
+                          <div style={{ marginTop: 8 }}>Upload</div>
+                        </div>
+                      )}
+                    </Upload>
+                  </div>
+                  <div className="flex justify-between">
+                    <h3>Add Mobile Banner (375 px by 181 px)</h3>
+                    <Upload
+                      action="/api/upload"
+                      listType="picture-card"
+                      fileList={fileLists.productImage}
+                      onChange={handleChange("productImage")}
+                      onPreview={handlePreviewImage}
+                    >
+                      {fileLists.productImage.length < 1 && (
+                        <div>
+                          <PlusOutlined />
+                          <div style={{ marginTop: 8 }}>Upload</div>
+                        </div>
+                      )}
+                    </Upload>
+                  </div>
+               
+                  <h2 className="text-xl font-bold">Additional Details
+                  </h2>
+                  <UploadAndViewVariationForm
+                    openedGame={openedGame}
+                    toggleForm={togleForm}
+                  />
                 </div>
               </div>
             </div>
           )}
         </div>
+        <Modal
+          open={previewVisible}
+          title="Image Preview"
+          footer={null}
+          onCancel={() => setPreviewVisible(false)}
+        >
+          <img alt="preview" style={{ width: "100%" }} src={previewImage} />
+        </Modal>
       </div>
     </div>
   );
--- src/pages/admin/admin-setup/active-variation/UploadAndViewVariationForm.tsx
+++ src/pages/admin/admin-setup/active-variation/UploadAndViewVariationForm.tsx
@@ -1,10 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { PlusOutlined } from "@ant-design/icons";
-import { Upload, Modal } from "antd";
+import { Upload, Modal, Button } from "antd";
 import type { UploadFile, UploadProps } from "antd";
 import { Config } from "../../../../services/Config";
+import { RcFile } from "antd/lib/upload/interface";
+import { Input, Form, Checkbox } from "antd";
 
-type FileType = Parameters<NonNullable<UploadProps['beforeUpload']>>[0];
+interface Option {
+  text: string;
+  image: string;
+  isCorrect: boolean;
+}
+
+interface Question {
+  text: string;
+  image: string;
+  options: Option[];
+}
+
+type FileType = Parameters<NonNullable<UploadProps["beforeUpload"]>>[0];
 
 const getBase64 = (file: FileType): Promise<string> =>
   new Promise((resolve, reject) => {
@@ -16,6 +30,7 @@ const getBase64 = (file: FileType): Promise<string> =>
 
 interface UploadAndViewVariationFormProps {
   openedGame: any;
+  toggleForm: string;
 }
 
 interface ImageUploadProps {
@@ -61,13 +76,11 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
 
 const UploadAndViewVariationForm: React.FC<UploadAndViewVariationFormProps> = ({
   openedGame,
+  toggleForm,
 }) => {
   const [previewOpen, setPreviewOpen] = useState(false);
   const [previewImage, setPreviewImage] = useState("");
   const [fileLists, setFileLists] = useState<{ [key: string]: UploadFile[] }>({
-    siteBanner: [],
-    mobileBanner: [],
-    productImage: [],
     backgroundImage: [],
     trainerBackgroundImage: [],
     playerBackgroundImage: [],
@@ -87,9 +100,25 @@ const UploadAndViewVariationForm: React.FC<UploadAndViewVariationFormProps> = ({
     img58: [],
   });
 
+  const [questions, setQuestions] = useState<Question[]>([
+    {
+      text: "",
+      image: "",
+      options: [
+        { text: "", image: "", isCorrect: false },
+        { text: "", image: "", isCorrect: false },
+        { text: "", image: "", isCorrect: false },
+        { text: "", image: "", isCorrect: false },
+      ],
+    },
+  ]);
+
+
+console.log(openedGame,"opened")
+
   useEffect(() => {
     const initialFileLists: { [key: string]: UploadFile[] } = {};
-    Object.keys(fileLists).forEach(key => {
+    Object.keys(fileLists).forEach((key) => {
       if (openedGame[key]) {
         initialFileLists[key] = [
           {
@@ -117,54 +146,284 @@ const UploadAndViewVariationForm: React.FC<UploadAndViewVariationFormProps> = ({
     setPreviewOpen(true);
   };
 
-  const handleChange = (imageType: string): UploadProps["onChange"] => ({ fileList: newFileList }) => {
-    setFileLists((prevFileLists) => ({
-      ...prevFileLists,
-      [imageType]: newFileList,
-    }));
-  };
+  const handleChange =
+    (imageType: string): UploadProps["onChange"] =>
+    ({ fileList: newFileList }) => {
+      setFileLists((prevFileLists) => ({
+        ...prevFileLists,
+        [imageType]: newFileList,
+      }));
+    };
 
   const handleCancelPreview = () => setPreviewOpen(false);
 
-  const imageFields = [
-    { label: "Site Banner (1280 px by 150 px)", imageType: "siteBanner" },
-    { label: "Add Product Image", imageType: "productImage" },
-    { label: "Add Mobile Banner (375 px by 181 px)", imageType: "mobileBanner" },
-    { label: "Add Board Background Image (1174 px by 790 px)", imageType: "backgroundImage" },
-    { label: "Add Trainer Background Image (1280 px by 720 px)", imageType: "trainerBackgroundImage" },
-    { label: "Add Player Background Image (750 px by 1280 px)", imageType: "playerBackgroundImage" },
-    { label: "Ladder 2 Pop up (790 px by 710 px)", imageType: "img2" },
-    { label: "Ladder 5 Pop up (790 px by 710 px)", imageType: "img6" },
-    { label: "Ladder 10 Pop up (790 px by 710 px)", imageType: "img12" },
-    { label: "Snake 13 Pop up (790 px by 710 px)", imageType: "img16" },
-    { label: "Ladder 17 Pop up (790 px by 710 px)", imageType: "img21" },
-    { label: "Snake 20 Pop up (790 px by 710 px)", imageType: "img25" },
-    { label: "Ladder 22 Pop up (790 px by 710 px)", imageType: "img28" },
-    { label: "Snake 26 Pop up (790 px by 710 px)", imageType: "img33" },
-    { label: "Snake 30 Pop up (790 px by 710 px)", imageType: "img38" },
-    { label: "Ladder 32 Pop up (790 px by 710 px)", imageType: "img41" },
-    { label: "Ladder 35 Pop up (790 px by 710 px)", imageType: "img45" },
-    { label: "Ladder 37 Pop up (790 px by 710 px)", imageType: "img48" },
-    { label: "Snake 39 Pop up (790 px by 710 px)", imageType: "img51" },
-    { label: "Snake 45 Pop up (790 px by 710 px)", imageType: "img58" },
-  ];
+  const handleInputChange = (
+    index: number,
+    type: "text" | "image",
+    value: string
+  ) => {
+    const newQuestions = [...questions];
+    newQuestions[index][type] = value;
+    setQuestions(newQuestions);
+  };
+
+  const handleOptionChange = (
+    qIndex: number,
+    optIndex: number,
+    type: "text" | "image",
+    value: string
+  ) => {
+    const newQuestions = [...questions];
+    newQuestions[qIndex].options[optIndex][type] = value;
+    setQuestions(newQuestions);
+  };
+
+  const handleFileChange = async (
+    index: number,
+    type: "image",
+    file: RcFile
+  ) => {
+    const base64 = await getBase64(file);
+    const newQuestions = [...questions];
+    newQuestions[index][type] = base64;
+    setQuestions(newQuestions);
+  };
+
+  const handleOptionFileChange = async (
+    qIndex: number,
+    optIndex: number,
+    type: "image",
+    file: RcFile
+  ) => {
+    const base64 = await getBase64(file);
+    const newQuestions = [...questions];
+    newQuestions[qIndex].options[optIndex][type] = base64;
+    setQuestions(newQuestions);
+  };
+
+  const handlePreviewQuestionImage = (file: string) => {
+    setPreviewImage(file);
+    setPreviewOpen(true);
+  };
+
+  const handleDeleteImage = (qIndex: number, optIndex?: number) => {
+    const newQuestions = [...questions];
+    if (optIndex === undefined) {
+      newQuestions[qIndex].image = "";
+    } else {
+      newQuestions[qIndex].options[optIndex].image = "";
+    }
+    setQuestions(newQuestions);
+  };
+
+  const handleCheckboxChange = (qIndex: number, optIndex: number) => {
+    const newQuestions = [...questions];
+    newQuestions[qIndex].options[optIndex].isCorrect =
+      !newQuestions[qIndex].options[optIndex].isCorrect;
+    setQuestions(newQuestions);
+  };
 
   return (
     <>
-      <div className="flex flex-col gap-6">
-        {imageFields.map((field, index) => (
-          <React.Fragment key={field.imageType}>
-            {index === 3 && <h2 className="text-lg font-bold">ADDITIONAL DETAILS</h2>}
-            <ImageUpload
-              label={field.label}
-              imageType={field.imageType}
-              fileList={fileLists[field.imageType] || []}
-              handleChange={handleChange(field.imageType)}
-              handlePreview={handlePreviewImage}
-            />
-          </React.Fragment>
-        ))}
-      </div>
+      {toggleForm === "snl" ? (
+        <div className="flex flex-col gap-6">
+          {[
+            {
+              label: "Add Board Background Image (1174 px by 790 px)",
+              imageType: "backgroundImage",
+            },
+            {
+              label: "Add Trainer Background Image (1280 px by 720 px)",
+              imageType: "trainerBackgroundImage",
+            },
+            {
+              label: "Add Player Background Image (750 px by 1280 px)",
+              imageType: "playerBackgroundImage",
+            },
+            { label: "Ladder 2 Pop up (790 px by 710 px)", imageType: "img2" },
+            { label: "Ladder 5 Pop up (790 px by 710 px)", imageType: "img6" },
+            {
+              label: "Ladder 10 Pop up (790 px by 710 px)",
+              imageType: "img12",
+            },
+            { label: "Snake 13 Pop up (790 px by 710 px)", imageType: "img16" },
+            {
+              label: "Ladder 17 Pop up (790 px by 710 px)",
+              imageType: "img21",
+            },
+            { label: "Snake 20 Pop up (790 px by 710 px)", imageType: "img25" },
+            {
+              label: "Ladder 22 Pop up (790 px by 710 px)",
+              imageType: "img28",
+            },
+            { label: "Snake 26 Pop up (790 px by 710 px)", imageType: "img33" },
+            { label: "Snake 30 Pop up (790 px by 710 px)", imageType: "img38" },
+            {
+              label: "Ladder 32 Pop up (790 px by 710 px)",
+              imageType: "img41",
+            },
+            {
+              label: "Ladder 35 Pop up (790 px by 710 px)",
+              imageType: "img45",
+            },
+            {
+              label: "Ladder 37 Pop up (790 px by 710 px)",
+              imageType: "img48",
+            },
+            { label: "Snake 39 Pop up (790 px by 710 px)", imageType: "img51" },
+            { label: "Snake 45 Pop up (790 px by 710 px)", imageType: "img58" },
+          ].map((field, index) => (
+            <React.Fragment key={field.imageType}>
+              <ImageUpload
+                label={field.label}
+                imageType={field.imageType}
+                fileList={fileLists[field.imageType] || []}
+                handleChange={handleChange(field.imageType)}
+                handlePreview={handlePreviewImage}
+              />
+            </React.Fragment>
+          ))}
+        </div>
+      ) : (
+        <div className="p-4">
+          {questions.map((question, qIndex) => (
+            <div key={qIndex} className="mb-8 p-4 border rounded-lg shadow-sm">
+              <h3 className="text-lg font-semibold mb-2">
+                Question {qIndex + 1}
+              </h3>
+              <Form layout="vertical">
+                <Form.Item label="">
+                  <div className="flex flex-row justify-between gap-4 items-center">
+                    <Input
+                      value={question.text}
+                      onChange={(e: any) =>
+                        handleInputChange(qIndex, "text", e.target.value)
+                      }
+                      className="h-8"
+                    />
+                    <Upload
+                      listType="picture-card"
+                      showUploadList={{
+                        showPreviewIcon: true,
+                        showRemoveIcon: true,
+                      }}
+                      onPreview={() =>
+                        handlePreviewQuestionImage(question.image)
+                      }
+                      onRemove={() => handleDeleteImage(qIndex)}
+                      beforeUpload={(file) => {
+                        handleFileChange(qIndex, "image", file);
+                        return false;
+                      }}
+                    >
+                      {question.image ? null : (
+                        <div>
+                          <PlusOutlined />
+                          <div style={{ marginTop: 8 }}>Upload</div>
+                        </div>
+                      )}
+                    </Upload>
+                  </div>
+                </Form.Item>
+
+                <div className="flex flex-col gap-4">
+                  {question.options.map((option, optIndex) => (
+                    <div
+                      key={optIndex}
+                      className="p-4 border rounded-lg shadow-sm flex items-center justify-between gap-4"
+                    >
+                      <Form.Item label="correct">
+                        <Checkbox
+                          checked={option.isCorrect}
+                          onChange={() =>
+                            handleCheckboxChange(qIndex, optIndex)
+                          }
+                        />
+                      </Form.Item>
+                      <Form.Item
+                        label={`Option ${optIndex + 1} Text`}
+                        className="flex-1"
+                      >
+                        <Input
+                          value={option.text}
+                          onChange={(e: any) =>
+                            handleOptionChange(
+                              qIndex,
+                              optIndex,
+                              "text",
+                              e.target.value
+                            )
+                          }
+                        />
+                      </Form.Item>
+                      <Form.Item>
+                        <Upload
+                          listType="picture-card"
+                          showUploadList={{
+                            showPreviewIcon: true,
+                            showRemoveIcon: true,
+                          }}
+                          onPreview={() =>
+                            handlePreviewQuestionImage(option.image)
+                          }
+                          onRemove={() => handleDeleteImage(qIndex, optIndex)}
+                          beforeUpload={(file) => {
+                            handleOptionFileChange(
+                              qIndex,
+                              optIndex,
+                              "image",
+                              file
+                            );
+                            return false;
+                          }}
+                        >
+                          {option.image ? null : (
+                            <div>
+                              <PlusOutlined />
+                              <div style={{ marginTop: 8 }}>Upload</div>
+                            </div>
+                          )}
+                        </Upload>
+                      </Form.Item>
+                    </div>
+                  ))}
+                </div>
+              </Form>
+            </div>
+          ))}
+          <div className="flex gap-4">
+            <Button
+              type="dashed"
+              onClick={() =>
+                setQuestions([
+                  ...questions,
+                  {
+                    text: "",
+                    image: "",
+                    options: [
+                      { text: "", image: "", isCorrect: false },
+                      { text: "", image: "", isCorrect: false },
+                      { text: "", image: "", isCorrect: false },
+                      { text: "", image: "", isCorrect: false },
+                    ],
+                  },
+                ])
+              }
+              icon={<PlusOutlined />}
+            >
+              Add Question
+            </Button>
+          </div>
+          <Modal
+            open={previewOpen}
+            title="Image Preview"
+            footer={null}
+            onCancel={handleCancelPreview}
+          >
+            <img alt="preview" style={{ width: "100%" }} src={previewImage} />
+          </Modal>
+        </div>
+      )}
       <Modal
         open={previewOpen}
         title="Image Preview"
